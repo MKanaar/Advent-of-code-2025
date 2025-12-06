e56a694bcf190bc701aad7aad4aab3eeb64636cc
MKanaar <Maartje.Kanaar@infosupport.com>
2025-12-06 10:53:41 +0100

    Dag 6

--- 06/script1.py
+++ 06/script1.py
@@ -1,6 +1,37 @@
 def parse():
-    output: list[tuple[int, int]] = []
-    file = open("./06/example.txt", "r")
-    input = file.read()
+    columns: list[list[int]] = []
+    operators: list[str] = []
+    file = open("./06/input.txt", "r")
+    for line in file.readlines():
+        elements = line.strip().split()
+        if elements[0] in ["+", "*"]:
+            operators = elements
+        else:
+            numbers = [int(x) for x in elements]
+            for i in range(len(numbers)):
+                if len(columns) <= i:
+                    columns.append([])
+                columns[i].append(numbers[i])
 
-    return output
+    return columns, operators
+
+
+def process(columns: list[list[int]], operators: list[str]) -> int:
+    total = 0
+    for i in range(len(operators)):
+        operator = operators[i]
+        if operator[0] == "+":
+            total += sum(columns[i])
+        elif operator[0] == "*":
+            prod = 1
+            for num in columns[i]:
+                prod *= num
+            total += prod
+
+    return total
+
+
+count = 0
+columns, operators = parse()
+count = process(columns, operators)
+print(count)
---
+++ 06/script2.py
@@ -0,0 +1,53 @@
+def parse():
+    grid: list[str] = []
+    operators: str = []
+    file = open("./06/input.txt", "r")
+    for line in file.readlines():
+        elements = line.strip("\n")
+        grid.append(elements)
+        if elements[0] in ["+", "*"]:
+            operators = elements.split()
+
+    operands: list[list[int]] = []
+    numbers: list[int] = []
+    number: str = ""
+    for col in range(len(grid[0])):
+        rows = len(grid)
+        for row in range(rows - 1, -1, -1):
+            char = grid[row][col]
+            if char.isdigit():
+                number += char
+
+            if row == 0 and number != "":
+                numbers.append(int(number[::-1]))
+                number = ""
+            elif row == rows - 1 and char in ["+", "*"]:
+                if not numbers:
+                    continue
+                operands.append(numbers)
+                numbers = []
+
+    operands.append(numbers)
+
+    return operands, operators
+
+
+def process(operands: list[list[int]], operators: list[str]) -> int:
+    total = 0
+    for i in range(len(operators)):
+        operator = operators[i]
+        if operator[0] == "+":
+            total += sum(operands[i])
+        elif operator[0] == "*":
+            prod = 1
+            for num in operands[i]:
+                prod *= num
+            total += prod
+
+    return total
+
+
+count = 0
+operands, operators = parse()
+count = process(operands, operators)
+print(count)
